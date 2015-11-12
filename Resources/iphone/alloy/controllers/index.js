874a2f260ebb9a28d6ee77f19cc81868c08f1d27
SunXiaomeng <12251216@life.hkbu.edu.hk>
2015-11-12 12:22:54 +0800

    Phase1

---
+++ Resources/iphone/alloy/controllers/index.js
@@ -0,0 +1,83 @@
+function __processArg(obj, key) {
+    var arg = null;
+    if (obj) {
+        arg = obj[key] || null;
+        delete obj[key];
+    }
+    return arg;
+}
+
+function Controller() {
+    require("alloy/controllers/BaseController").apply(this, Array.prototype.slice.call(arguments));
+    this.__controllerPath = "index";
+    this.args = arguments[0] || {};
+    if (arguments[0]) {
+        {
+            __processArg(arguments[0], "__parentSymbol");
+        }
+        {
+            __processArg(arguments[0], "$model");
+        }
+        {
+            __processArg(arguments[0], "__itemTemplate");
+        }
+    }
+    var $ = this;
+    var exports = {};
+    var __alloyId0 = [];
+    $.__views.__alloyId2 = Ti.UI.createWindow({
+        backgroundColor: "#fff",
+        title: "Tab 1",
+        id: "__alloyId2"
+    });
+    $.__views.__alloyId3 = Ti.UI.createWebView({
+        url: "/webcontent/webpage/login.html",
+        id: "__alloyId3"
+    });
+    $.__views.__alloyId2.add($.__views.__alloyId3);
+    $.__views.__alloyId1 = Ti.UI.createTab({
+        window: $.__views.__alloyId2,
+        title: "Tab 1",
+        icon: "KS_nav_ui.png",
+        id: "__alloyId1"
+    });
+    __alloyId0.push($.__views.__alloyId1);
+    $.__views.__alloyId5 = Ti.UI.createWindow({
+        backgroundColor: "#fff",
+        title: "Tab 2",
+        id: "__alloyId5"
+    });
+    $.__views.__alloyId6 = Ti.UI.createLabel({
+        width: Ti.UI.SIZE,
+        height: Ti.UI.SIZE,
+        color: "#000",
+        font: {
+            fontSize: 20,
+            fontFamily: "Helvetica Neue"
+        },
+        textAlign: "center",
+        text: "I am Window 2",
+        id: "__alloyId6"
+    });
+    $.__views.__alloyId5.add($.__views.__alloyId6);
+    $.__views.__alloyId4 = Ti.UI.createTab({
+        window: $.__views.__alloyId5,
+        title: "Tab 2",
+        icon: "KS_nav_views.png",
+        id: "__alloyId4"
+    });
+    __alloyId0.push($.__views.__alloyId4);
+    $.__views.index = Ti.UI.createTabGroup({
+        tabs: __alloyId0,
+        id: "index"
+    });
+    $.__views.index && $.addTopLevelView($.__views.index);
+    exports.destroy = function() {};
+    _.extend($, $.__views);
+    $.index.open();
+    _.extend($, exports);
+}
+
+var Alloy = require("alloy"), Backbone = Alloy.Backbone, _ = Alloy._;
+
+module.exports = Controller;
---
+++ Resources/iphone/webcontent/scripts/main.js
@@ -0,0 +1,7 @@
+function submit() {
+    window.location.href = "mainPage.html";
+}
+
+function register() {
+    window.location.href = "registerPage.html";
+}
